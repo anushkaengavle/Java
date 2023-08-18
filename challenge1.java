1d9674634cfb7cb5bf322c85dca35c6732a0a0b7
Anushka Engavle <anushkaengavle@gmail.com>
2023-08-18 16:56:40 +0530

    Update challenge1.java

--- challenge1.java
+++ challenge1.java
@@ -3,9 +3,9 @@ public class challenge1 {
         double var1 = 20.00;
         double var2 = 80.00;
         double add = (var1+var2)*100.00;
-        // System.out.println("The addition of the 2 variable multiplied by 100.00 is "+add);    
+        System.out.println("The addition of the 2 variable multiplied by 100.00 is "+add);    
         double rem = add % 40.00 ;
-        // System.out.println("The remainder is " +rem);
+        System.out.println("The remainder is " +rem);
         Boolean x = (rem == 0.00); 
         // Boolean x = rem==0.00 ? true : false ;//other way 
 
